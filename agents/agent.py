8ff9985f665c7ef40ea7a78e0e4c745458cd4846
reoneo97 <reoneo97@gmail.com>
2020-08-28 20:49:36 +0800

    removed hi

--- agents/agent.py
+++ agents/agent.py
@@ -1,6 +1,7 @@
 class Agent:
-    def __init__(self,income):
+    def __init__(self,income,prob):
         self.income = income
         self.prob = prob
-    def move():
+        self.path = None
+    def move(self):
         pass
