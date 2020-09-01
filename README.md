1508be485ea121921e9c35099fd77198cb9a5ce5
reoneo97 <reoneo97@gmail.com>
2020-09-01 11:02:58 +0800

    solved djikstra algo

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # transport-abm
 Project using an Agent-based Modelling approach to analyse traffic congestion in Singapore
+
+
+
+### File Structure
+├───agents
+├───cfg
+└───env
--- main.py
+++ main.py
@@ -4,15 +4,26 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-def create_graph(path = "./cfg/locations.csv"):
+def create_env(path = "./cfg/locations.csv"):
+    #Function to create the network 
+    graph = Graph()
     df = pd.read_csv(path)
+    locations = set(df["loc1"].tolist()+df["loc2"].tolist())
+    locations = {i:Location(i) for i in locations}
+    for i in df.values.tolist():
+        graph.add_edge(locations[i[0]],locations[i[1]],i[2])
+    env = Environment(graph, locations)
+    return env
+
+        
+
+
+if __name__ == "__main__": 
+    gp = create_env()
+    # print(gp.graph.apsp())
+    # print(gp.graph.nodes)
+    print(gp.graph.data)
+    gp.graph.shortest_path("Tampines")
     
-    for i in df.iterrows():
-        print(i)
-        print("yo")
 
-
-if __name__ == "__main__":
-    hi = Graph()
-    create_graph()
 
