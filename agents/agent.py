ae3382f68bd29ebeca6449197db210ce6d6f8eeb
reoneo97 <reoneo97@gmail.com>
2020-09-02 22:40:55 +0800

    updated readme

--- agents/agent.py
+++ agents/agent.py
@@ -1,11 +1,17 @@
 class Agent:
-    def __init__(self,env,income,prob,home,dest):
+    def __init__(self,env,income,prob,home,dest,work_time):
         self.income = income
         self.prob = prob
         self.home = home
         loc2idx = env.loc2idx
         self.dest = dest
+
         self.path_to_dest = env.paths[home][loc2idx[dest]]
         self.path_to_home = env.paths[dest][loc2idx[home]]
+        self.env = env
     def move(self):
+        
+        pass
+    def update(self):
+        
         pass
--- main.py
+++ main.py
@@ -9,19 +9,25 @@ def create_env(path = "./cfg/locations.csv"):
     graph = Graph()
     df = pd.read_csv(path)
     locations = set(df["loc1"].tolist()+df["loc2"].tolist())
-    locations = {i:Location(i) for i in locations}
+    loc_dict = {i:Location(i) for i in locations}
+    locations = list(locations)
     for i in df.values.tolist():
-        graph.add_edge(locations[i[0]],locations[i[1]],i[2])
-    env = Environment(graph, locations)
+        graph.add_edge(loc_dict[i[0]],loc_dict[i[1]],i[2])
+        transit1 = i[0]+"->"+i[1]
+        transit2 = i[1]+"->"+i[0]
+        locations.append(Location(transit1))
+        locations.append(Location(transit2))
+    config = ""
+    env = Environment(graph, locations,config)
     return env
         
 
 
 if __name__ == "__main__": 
     env = create_env()
-    a = Agent(env,2000,0.5,"Bishan","Tiong Bahru")
+    a = Agent(env,2000,0.5,"Bishan","Tiong Bahru",time(hour = 7, minute = 30))
     print(a.path_to_dest)
     print(a.path_to_home)
-    
+    print([str(i) for i in env.locations])
 
 
