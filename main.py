6d1742e6e58abeab4b6ebda362a4e15d82c960b5
reoneo97 <reoneo97@gmail.com>
2020-09-03 18:31:24 +0800

    added utils

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from agents.agent import *
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from utils import *
 
 def create_env(path = "./cfg/locations.csv"):
     #Function to create the network 
@@ -15,8 +16,8 @@ def create_env(path = "./cfg/locations.csv"):
         graph.add_edge(loc_dict[i[0]],loc_dict[i[1]],i[2])
         transit1 = i[0]+"->"+i[1]
         transit2 = i[1]+"->"+i[0]
-        locations.append(Location(transit1))
-        locations.append(Location(transit2))
+        locations.append(Location(transit1,True))
+        locations.append(Location(transit2,True))
     config = ""
     env = Environment(graph, locations,config)
     return env
@@ -25,6 +26,7 @@ def create_env(path = "./cfg/locations.csv"):
 
 if __name__ == "__main__": 
     env = create_env()
+    print(env.locations)
     a = Agent(env,2000,0.5,"Bishan","Tiong Bahru",time(hour = 7, minute = 30))
     print(a.path_to_dest)
     print(a.path_to_home)
